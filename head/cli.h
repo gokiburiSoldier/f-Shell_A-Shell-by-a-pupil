c086c422a60cb68c91c045a9e85159153061d65b
gokiburiSoldier 小号 <gokiburisoldier@outlook.com>
2024-08-05 15:32:23 +0800

    重构了

--- head/cli.h
+++ head/cli.h
@@ -16,11 +16,13 @@
 #include "fsl/rw.h"
 #include "admin.h"
 #include "fsl/calc.h"
+#include "get_code.h"
 using namespace std;
 
 bool ctrl_c(DWORD signal) {
     return 0;
 }
+
 
 namespace run {
     map<string,string> vars;
@@ -34,6 +36,10 @@ namespace run {
         getline(cin,s);
         cout << "\033[0m";
         return s;
+    }
+    bool isVar(string s) {
+        int len = s.length();
+        return s[0] == '$' && s[len-1] == '$' || s[0] == '@' && s[len-1] == '@';
     }
     vector<string> spliter(string sent) {
         sent += " ";
@@ -84,81 +90,108 @@ namespace run {
             }
         }
         if(t != "") rt.push_back(t);
+        int len2 = rt.size();
+        for(int i=1;i<len2;++i) 
+            if(rt[0] != "rd" && 
+               rt[0] != "df" && 
+               isVar(rt[i])  && 
+               vars.count(rt[i])
+            )  rt[i] = vars[rt[i]];
         return rt;
     }
 
     int run(vector<string> t) {
         if(t.empty()) return 0;
-        string fsl = t[0];
-        if(fsl == "cd") {
-            if(t.size() == 1) return -1;
-            string adr2;
-            adr2 = adr+'\\'+(eh::is_str(t[1]) ? eh::str_no_quot(t[1]) : t[1]);
-            if(cd::exist_floder(adr2)) {
-                if(t[1] == "..") {
-                    int len = adr.length(),i;
-                    if(len == 2) return 0;
-                    for(i=1;adr[len-i] != '\\';++ i) adr = adr.substr(0,len-i);
-                    adr = adr.substr(0,len-i);
-                }else if(t[1] == "/") {
-                    adr = adr.substr(0,2);
-                }else adr = adr2;
-                chdir(adr.c_str());
-            }else return 2;
-        } else if(fsl == "ls") ls::ls(adr);
-        else if(fsl == "echo") eh::echo(t);
-        else if(fsl == "cat") {
-            if(t.size() == 1) return -1;
-            return rw::typing(adr+"\\"+t[1]);
-        } else if(fsl == "cls" || fsl == "clear") system("cls");
-        else if(fsl == "quit" || fsl == "exit");
-        else if(fsl == "wt") {
-            string a;
-            int len = t.size();
-            for(int i=1;i<len;++i) {
-                if(t[i] == "-a") {
-                    if(i == len-1) return -1;
-                    a = t[i+1];
-                    break;
+        string fsl = t[0],adr2,a,c;
+        int len;
+        switch(getcode(fsl)) {
+            case fff::cd:
+                if(t.size() == 1) return -1;
+                adr2 = adr+'\\'+(eh::is_str(t[1]) ? eh::str_no_quot(t[1]) : t[1]);
+                if(cd::exist_floder(adr2)) {
+                    if(t[1] == "..") {
+                        int len = adr.length(),i;
+                        if(len == 2) return 0;
+                        for(i=1;adr[len-i] != '\\';++ i) adr = adr.substr(0,len-i);
+                        adr = adr.substr(0,len-i);
+                    }else if(t[1] == "/") {
+                        adr = adr.substr(0,2);
+                    }else adr = adr2;
+                    chdir(adr.c_str());
+                }else return 2;
+                break;
+            case fff::ls: 
+                ls::ls(adr);
+                break;
+            case fff::echo: 
+                eh::echo(t);
+                break;
+            case fff::cat:
+                if(t.size() == 1) return -1;
+                return rw::typing(adr+"\\"+t[1]);
+                break;
+            case fff::cls:
+            case fff::clear:
+                system("cls");
+                break;
+            case fff::quit:
+            case fff::exit:
+                break;
+            case fff::wt: 
+                len = t.size();
+                for(int i=1;i<len;++i) {
+                    if(t[i] == "-a") {
+                        if(i == len-1) return -1;
+                        a = t[i+1];
+                        break;
+                    }
                 }
-            }
-            if(a == "") return -1;
-            return rw::writing(adr+"\\"+a,t,a);
+                if(a == "") return -1;
+                return rw::writing(adr+"\\"+a,t,a);
+                break;
+            case fff::sd:
+            case fff::shutdown: 
+                c = "shutdown ";
+                len = t.size();
+                for(int i=1;i<len;++ i) c += t[i]+" ";
+                system(c.c_str());
+                break;
+            case fff::help: 
+                if(t.size() < 2) return -1;
+                return rw::typing("D:\\fShell\\help\\"+t[1]+".txt");
+                break;
+            case fff::s_add: 
+                if(t.size() < 3) return -1;
+                else             cout << cl::add(t[1],t[2]) << endl;
+                break;
+            case fff::s_sub: 
+                if(t.size() < 3) return -1;
+                if(cl::compare(t[1],t[2])) cout << "-" << cl::subtra(t[2],t[1]) << endl;
+                else                       cout << cl::subtra(t[1],t[2]) << endl;
+                break;
+            case fff::df: 
+                if(t.size() < 3) return -1;
+                vars[t[1]] = t[2];
+                break;
+            case fff::rd: 
+                if(t.size() < 2) return -1;
+                else if(!vars.count(t[1])) return 3;
+                cout << vars[t[1]] << endl;
+                break;
+            case fff::cp: 
+                if(t.size() < 3) return -1;
+                rw::cp(t[1],t[2]);
+                break;
+            case fff::s_mul: 
+                if(t.size() < 3) return -1;
+                cout << cl::mul(t[1],t[2]) << endl;
+                break;
+            default:
+                if(fsl.length() == 2 && fsl[1] == ':') adr = fsl;
+                else return 1;
+                break;
         }
-        else if(fsl == "sd" || fsl == "shutdown") {
-            string c = "shutdown ";
-            int len = t.size();
-            for(int i=1;i<len;++ i) c += t[i]+" ";
-            system(c.c_str());
-        }
-        else if(fsl.length() == 2 && fsl[1] == ':') adr = fsl;
-        else if(fsl == "help") {
-            if(t.size() < 2) return -1;
-            return rw::typing("D:\\fShell\\help\\"+t[1]+".txt");
-        }
-        else if(fsl == "+") {
-            if(t.size() < 3) return -1;
-            else             cout << cl::add(t[1],t[2]) << endl;
-        }else if(fsl == "-") {
-            if(t.size() < 3) return -1;
-            if(cl::compare(t[1],t[2])) cout << "-" << cl::subtra(t[2],t[1]) << endl;
-            else                       cout << cl::subtra(t[1],t[2]) << endl;
-        }else if(fsl == "df") {
-            if(t.size() < 3) return -1;
-            vars[t[1]] = t[2];
-        }else if(fsl == "rd") {
-            if(t.size() < 2) return -1;
-            else if(!vars.count(t[1])) return 3;
-            cout << vars[t[1]] << endl;
-        }else if(fsl == "cp") {
-            if(t.size() < 3) return -1;
-            rw::cp(t[1],t[2]);
-        }else if(fsl == "*") {
-            if(t.size() < 3) return -1;
-            cout << cl::mul(t[1],t[2]) << endl;
-        }
-        else return 1;
-        
+        if(fsl != "cls" && fsl != "clear") cout << "\n";
         return 0;
     }
 } 
--- head/get_code.h
+++ head/get_code.h
@@ -23,7 +23,7 @@ char anay(char c) { /* 为什么不用`char &c` 因为不习惯 */
     return c;
 }
 
-long long getcode(std::string str) {
+const long long getcode(std::string str) {
     int len = str.length();
     long long sum=0,cnt=0; /* sum处理每个字符取模114514,cnt计算除以114514 */
     if(len >= sizeof(primes)) return -1; /* 字符串过长 */
@@ -36,5 +36,22 @@ long long getcode(std::string str) {
     return (sum/(cnt+1)) ^ (sum%(cnt+1));
 }
 
+namespace fff {
+    const int cd = 172459,ls = 79955,
+            echo = 311643,
+            cat = 280486,
+            cls = 252216,
+            clear = 436500,
+            quit = 297641,
+            exit = 200859,
+            wt = 167135,
+            sd = 172281,
+            shutdown = 538447,
+            s_add = 24374,
+            s_sub = 97496,
+            s_mul = 69444,
+            df = 133267,rd = 177357,
+            cp = 118505,help = 179707;
+}
 
 #endif
